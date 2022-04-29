da712dae7f53904d4448500bb5477faf9da0fbfe
Vezunchic <piratAlexander@yandex.ru>
2022-04-29 22:29:49 +0300

    Добавлено задание к 3 уроку

---
+++ DistanceCalculation/DistanceCalculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace DistanceCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Начало программы");
+           
+            string path = Environment.CurrentDirectory;
+            var result = BenchmarkRunner.Run<BechmarkClass>();
+           
+       
+        }
+
+
+    }
+}
